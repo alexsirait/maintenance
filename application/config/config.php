905ccba9330850db136781382bbe011cc3ebf664
Aji Utomo <ajiutomo08@gmail.com>
2022-11-30 11:42:05 +0700

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/maintenance/';
+// $config['base_url'] = 'http://localhost/maintenance/';
+$config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'] .
+str_replace(
+    basename($_SERVER['SCRIPT_NAME']),
+    "",
+    $_SERVER['SCRIPT_NAME']
+);
 
 /*
 |--------------------------------------------------------------------------
