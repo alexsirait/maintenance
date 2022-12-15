7adba1578b73765fc0f1224b38edd018345ce408
Aji Utomo <ajiutomo08@gmail.com>
2022-12-15 09:26:52 +0700

    Update Dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -108,96 +108,11 @@
               </div><!-- /.card-header -->
               <div class="card-body">
                 <div class="tab-content p-0">
-
                   <!-- Start Chart -->                  
                   <div id="chartContainer1" style="height: 300px; width: 100%; position: relative;"></div>                  
                   <!-- End Chart -->
-  
                 </div>
               </div><!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-
-            <!-- Map card -->
-            <div class="card bg-gradient-primary">
-              <div class="card-header border-0">
-                <h3 class="card-title">
-                  <i class="fas fa-map-marker-alt mr-1"></i>
-                  Visitors
-                </h3>
-              </div>
-              <div class="card-body">
-                <div id="world-map" style="height: 250px; width: 100%;"></div>
-              </div>
-              <!-- /.card-body-->
-              <div class="card-footer bg-transparent">
-                <div class="row">
-                  <div class="col-4 text-center">
-                    <div id="sparkline-1"></div>
-                    <div class="text-white">Visitors</div>
-                  </div>
-                  <!-- ./col -->
-                  <div class="col-4 text-center">
-                    <div id="sparkline-2"></div>
-                    <div class="text-white">Online</div>
-                  </div>
-                  <!-- ./col -->
-                  <div class="col-4 text-center">
-                    <div id="sparkline-3"></div>
-                    <div class="text-white">PPM vs Actual</div>
-                  </div>
-                  <!-- ./col -->
-                </div>
-                <!-- /.row -->
-              </div>
-            </div>
-            <!-- /.card -->
-
-          </section>
-          <!-- /.Left col -->
-          <!-- right col (We are only adding the ID to make the widgets sortable)-->
-          <section class="col-lg-5 connectedSortable">    
-
-            <!-- solid sales graph -->
-            <div class="card bg-gradient-info">
-              <div class="card-header border-0">
-                <h3 class="card-title">
-                  <i class="fas fa-th mr-1"></i>
-                  Master List of Equipment
-                </h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
-              </div>
-              <div class="card-body">
-
-                <!-- Start Chart -->
-                <div id="chartContainer2" style="height: 300px; width: 100%;"></div>
-                <!-- End Chart -->
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer bg-transparent">
-                <div class="row">
-                  <div class="col-6 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="<?= $hsumpretentive_calibration ?>" data-width="60" data-height="60" data-fgColor="#39CCCC">
-                    <div class="text-white">Pretentive & Calibration</div>
-                  </div>
-                  <!-- ./col -->
-                  <div class="col-6 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="<?= $hsumppm ?>" data-width="60" data-height="60" data-fgColor="#39CCCC">
-                    <div class="text-white">Pretentive</div>
-                  </div>
-                  <!-- ./col -->
-                </div>
-                <!-- /.row -->
-              </div>
-              <!-- /.card-footer -->
             </div>
             <!-- /.card -->
 
@@ -240,6 +155,99 @@
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
+
+            <!-- Map card -->
+            <div class="card bg-gradient-primary" hidden>
+              <div class="card-header border-0">
+                <h3 class="card-title">
+                  <i class="fas fa-map-marker-alt mr-1"></i>
+                  Visitors
+                </h3>
+                <!-- card tools -->
+                <div class="card-tools">
+                  <button type="button" class="btn btn-primary btn-sm daterange" title="Date range">
+                    <i class="far fa-calendar-alt"></i>
+                  </button>
+                  <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <div class="card-body">
+                <div id="world-map" style="height: 250px; width: 100%;"></div>
+              </div>
+              <!-- /.card-body-->
+              <div class="card-footer bg-transparent">
+                <div class="row">
+                  <div class="col-4 text-center">
+                    <div id="sparkline-1"></div>
+                    <div class="text-white">Visitors</div>
+                  </div>
+                  <!-- ./col -->
+                  <div class="col-4 text-center">
+                    <div id="sparkline-2"></div>
+                    <div class="text-white">Online</div>
+                  </div>
+                  <!-- ./col -->
+                  <div class="col-4 text-center">
+                    <div id="sparkline-3"></div>
+                    <div class="text-white">PPM vs Actual</div>
+                  </div>
+                  <!-- ./col -->
+                </div>
+                <!-- /.row -->
+              </div>
+            </div>
+            <!-- /.card -->
+
+          </section>
+          <!-- /.Left col -->
+          <!-- right col (We are only adding the ID to make the widgets sortable)-->
+          <section class="col-lg-5 connectedSortable">    
+
+            <!-- solid sales graph -->
+            <div class="card bg-gradient-info">
+              <div class="card-header border-0">
+                <h3 class="card-title">
+                  <i class="fas fa-th mr-1"></i>
+                  Cladtek Maintenance
+                </h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+                <!-- Start Chart -->
+                <div id="chartContainer2" style="height: 300px; width: 100%;"></div>
+                <!-- End Chart -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer bg-transparent">
+                <div class="row">
+                  <div class="col-6 text-center">
+                    <input type="text" class="knob" data-readonly="true" value="<?= $hsumpretentive_calibration ?>" data-width="60" data-height="60" data-fgColor="#39CCCC">
+                    <div class="text-white">Pretentive & Calibration</div>
+                  </div>
+                  <!-- ./col -->
+                  <div class="col-6 text-center">
+                    <input type="text" class="knob" data-readonly="true" value="<?= $hsumppm ?>" data-width="60" data-height="60" data-fgColor="#39CCCC">
+                    <div class="text-white">Pretentive</div>
+                  </div>
+                  <!-- ./col -->
+                </div>
+                <!-- /.row -->
+              </div>
+              <!-- /.card-footer -->
+            </div>
+            <!-- /.card -->
+            
           </section>
           <!-- right col -->
         </div>
@@ -253,6 +261,7 @@
     window.onload = function () {
         var chart1 = new CanvasJS.Chart("chartContainer2",
         {
+          // theme: "light2",
           title:{
           text: "PPM vs Actual"  
           },
