a978a33a7375ccd47005a5dac3edf6d1994ebeed
ALEX SIRAIT <alexsirait1001@gmail.com>
2022-12-15 11:05:21 +0700

    UPDATE MAINTENANCE

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,5 +1,5 @@
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
